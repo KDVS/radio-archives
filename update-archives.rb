bde28ec9568372d85d27c22cd904161f5098e33c
Christopher Thielen <cthielen@gmail.com>
2012-06-17 17:48:21 -0700

    Beginnings of Ruby-based rewrite

---
+++ update-archives.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+# update-archives.rb: Creates or updates the cron jobs which generate/maintain the archives
+#
+# Version 0.1 (2012-06-17)
+# Written by Christopher Thielen (cthielen@gmail.com)
+
+schedule_url = "http://library.kdvs.org/ajax/streamingScheduleJSON"
