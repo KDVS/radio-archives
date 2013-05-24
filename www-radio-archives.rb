4f1d4ebfdc11c1aec37fab4fdf45930bd65ea8bc
Christopher Thielen <chris@epiar.net>
2013-05-24 01:54:11 -0700

    Minor update to www interface

--- www-radio-archives.rb
+++ www-radio-archives.rb
@@ -10,12 +10,14 @@ Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file}
 
 get '/' do
   html = "<table>"
+  html += "<thead><tr><td>Title</td><td>Artist</td><td># Recordings</td></tr></thead>"
+  html += "<tbody>"
   Show.all.each do |s|
     html += "<tr>"
-    html += "<td>#{s.title}</td><td>#{s.artist}</td>"
+    html += "<td>#{s.title}</td><td>#{s.artist}</td><td>#{s.recordings.length}</td>"
     html += "</tr>"
   end
-  html += "</table>"
+  html += "</tbody></table>"
   return html
 end
 
